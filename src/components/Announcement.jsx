a2e49af28efb01b5cffab588cdd4f29892058751
OritLeshem <azmono@gmail.com>
2021-11-18 20:49:37 -0500

    Announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import styled from "styled-components";
+const Container = styled.div`
+  background-color: pink;
+  color: white;
+  height: 35px;
+  display: flex;
+  font-weight: 900;
+  justify-content: center;
+  align-items: center;
+`;
 const Announcement = () => {
-  return <div></div>;
+  return <Container>FREE SHIPPING IN CANADA !</Container>;
 };
 
 export default Announcement;
